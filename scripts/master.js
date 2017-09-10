aea9b8f7db3cae905497f0a164579c9a30c18f8a
nhayden <6475155+nhayden@users.noreply.github.com>
2017-09-10 16:13:49 -0700

    complete animation for hide filters

--- scripts/master.js
+++ scripts/master.js
@@ -1,22 +1,32 @@
 $(function() {
+  $(".filter-items").click(function(e) {
+    e.preventDefault(); // don't let clicking filters trigger jumping
+  })
   $(":button").click(function() {
     const ELT = $('.gallery a:nth-child(3)');
     //console.log(ELT);
     ELT.toggleClass("hideThumb");
   });
-  $(".minimizerButton").click(function() {
-    const _this = $(this);
-    // console.log(_this);
-    if (_this.hasClass("fa-minus-square-o")) {
-      _this.removeClass("fa-minus-square-o");
-      _this.addClass("fa-plus-square-o");
+  let filtersCollapsed = false;
+  $("#minimize-filters").click(function(e) {
+    e.preventDefault();
+    // toggle button and text
+    const theButton = $("#minimize-filters-button");
+    if (!filtersCollapsed) {
+      theButton.removeClass("fa-minus-square-o");
+      theButton.addClass("fa-plus-square-o");
+      $("#minimize-filters-text").text("show filtering options")
     } else {
-      _this.removeClass("fa-plus-square-o");
-      _this.addClass("fa-minus-square-o");
+      theButton.removeClass("fa-plus-square-o");
+      theButton.addClass("fa-minus-square-o");
+      $("#minimize-filters-text").text("hide filtering options")
     }
-  });
-  $("#minimize-filters").click(function() {
+
+    // hide filtering div
     const filterDiv = $(".filtering");
     filterDiv.toggleClass("hideFiltering");
+
+    filtersCollapsed = !filtersCollapsed;
+
   });
 });
