162c906fc6c455c7e782587469c0eee46c88cbba
nhayden <6475155+nhayden@users.noreply.github.com>
2017-10-19 21:59:32 -0700

    update to include bounder

--- tag_generator.py
+++ tag_generator.py
@@ -3,4 +3,4 @@
 with open("sample_wallpapers_list.txt", "r") as f:
 	for line in f:
 		full, thumb = line.strip().split(",")
-		print('<div class="thumb-box"><a class="full-anchor" href="{}"><img class="thumb" data-fullres="{}" src="{}"></a></div>'.format(full, full, thumb))
+		print('<div class="thumb-box"><div class="bounder"><a class="full-anchor" href="{}"><img class="thumb thumb-low-res" data-fullres="{}" src="{}"></a><div class="thumb-info"><span class="res">placeholder</span></div></div></div>'.format(full, full, thumb))
